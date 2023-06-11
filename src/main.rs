853f847f579f02558a424af138797250946a3ab7
hrrs01 <haavard.hn@gmail.com>
2023-06-11 17:01:14 +0200

    travel

--- src/main.rs
+++ src/main.rs
@@ -26,50 +26,36 @@ use rp_pico::pac::SPI0;
 use rtt_target::rprintln;
 use rtt_target::rtt_init_print;
 
+use display_interface::WriteOnlyDataCommand;
+use display_interface::DataFormat;
 use display_interface_spi::SPIInterface;
+
 
 use fugit::RateExtU32;
 
 
-const ILI9468_CASET: u16 = 0x2A;
-const ILI9468_PASET: u16 = 0x2B;
-const ILI9468_RAMWR: u16 = 0x2C;
-const ILI9468_RAMRD: u16 = 0x2E;
-const ILI9468_MADCTL: u16 = 0x36;
+const ILI9468_CASET: u8 = 0x2A;
+const ILI9468_PASET: u8 = 0x2B;
+const ILI9468_RAMWR: u8 = 0x2C;
+const ILI9468_RAMRD: u8 = 0x2E;
+const ILI9468_MADCTL: u8 = 0x36;
+const ILI9468_MAD_BGR: u8 = 0x08;
+const ILI9468_MAD_MX: u8 = 0x40;
 
 
-fn start_spi<I>(cs: &mut Pin<I, Output<PushPull>>)
-    where
-        I: PinId
+
+fn spi_write(di: &mut impl WriteOnlyDataCommand, bc: &[u16])
 {
-    cs.set_low().unwrap();
+
+    di.send_data(DataFormat::U16(bc)).unwrap();
+
 }
 
-fn stop_spi<I>(cs: &mut Pin<I, Output<PushPull>>)
-    where
-        I: PinId
-{
-    cs.set_high().unwrap();
-}
-
-fn spi_write<S, I, Y>(di: & SPIInterface<Spi<Enabled, S, 8>, Pin<I, Output<PushPull>>, Pin<Y, Output<PushPull>>>, bc: u8)
-    where
-        S: SpiDevice,
-        I: PinId,
-        Y: PinId,
-{
-    // spi.write(&[bc]).unwrap();
-}
-
-fn command_write<S, I, Y>(di: & SPIInterface<Spi<Enabled, S, 8>, Pin<I, Output<PushPull>>, Pin<Y, Output<PushPull>>>, bc: u8)
-where
-    S: SpiDevice,
-    I: PinId,
-    Y: PinId,
-
+fn command_write(di: &mut impl WriteOnlyDataCommand , bc: u8)
 {   
     rprintln!("Writing command.");
     // spi.write(&[bc]).unwrap_or_else(|err| { rprintln!("Error!"); });
+    di.send_commands(DataFormat::U8(&[bc])).unwrap();
 
 }
 
@@ -137,104 +123,89 @@ fn main() -> ! {
     // 20MHz is max frequency for this screen.
     let mut spi = spi.init(&mut pac.RESETS, clocks.peripheral_clock.freq(), 10.MHz(), &embedded_hal::spi::MODE_3);
 
-    let di = SPIInterface::new(spi, dc_pin, cs_pin);
+    let mut di = SPIInterface::new(spi, dc_pin, cs_pin);
 
     // Try to initalize screen here:
 
     
     // Do a soft reset here:
-    command_write(&di, 0x01);
+    command_write(&mut di, 0x01);
+    delay.delay_ms(120);
+
+    command_write(&mut di, 0x11);
     delay.delay_ms(120);
 
     // Pixel Format
-    command_write(&di, 0x3A);
-    spi_write(&di, 0x55);
+    command_write(&mut di, 0x3A);
+    spi_write(&mut di, &[0x55]);
 
-    // Memory format
-    command_write(&di, 0x36);
-    spi_write(&di, 0x48);
+    // Power Control 1
+    command_write(&mut di, 0xC0);
+    spi_write(&mut di, &[0x0E, 0x0E]);
+
+    // Power Control 2
+    command_write(&mut di, 0xC1);
+    spi_write(&mut di, &[0x41, 0x00]);
+
+    // Power Control 3
+    command_write(&mut di, 0xC2);
+    spi_write(&mut di, &[0x55]);
+
 
     // Positive Gamma control (PGAM CTRL)
-    command_write(&di, 0xE0);
-    spi_write(&di, 0x0F);
-    spi_write(&di, 0x1F);
-    spi_write(&di, 0x1C);
-    spi_write(&di, 0x0C);
-    spi_write(&di, 0x0F);
-    spi_write(&di, 0x08);
-    spi_write(&di, 0x48);
-    spi_write(&di, 0x98);
-    spi_write(&di, 0x37);
-    spi_write(&di, 0x0A);
-    spi_write(&di, 0x13);
-    spi_write(&di, 0x04);
-    spi_write(&di, 0x11);
-    spi_write(&di, 0x0D);
-    spi_write(&di, 0x00);
+    let pgam_ctrl: [u16; 16] = [0x0F, 0x1F, 0x1C, 0x0C, 0x0F, 0x08, 0x48, 0x98, 0x37, 0x0A, 0x13, 0x04, 0x11, 0x11, 0x0D, 0x00];
+    command_write(&mut di, 0xE0);
+    spi_write(&mut di, &pgam_ctrl);
 
     // Negative Gamma control (NGAM CTRL)
-    command_write(&di, 0xE1);
-    spi_write(&di, 0x0F);
-    spi_write(&di, 0x32);
-    spi_write(&di, 0x2E);
-    spi_write(&di, 0x0B);
-    spi_write(&di, 0x0D);
-    spi_write(&di, 0x05);
-    spi_write(&di, 0x47);
-    spi_write(&di, 0x75);
-    spi_write(&di, 0x37);
-    spi_write(&di, 0x06);
-    spi_write(&di, 0x10);
-    spi_write(&di, 0x03);
-    spi_write(&di, 0x24);
-    spi_write(&di, 0x20);
-    spi_write(&di, 0x00);
-
+    command_write(&mut di, 0xE1);
+    let ngam_ctrl: [u16; 15] = [0x0F, 0x32, 0x2E, 0x0B, 0x0D, 0x05, 0x47, 0x75, 0x37, 0x06, 0x10, 0x03, 0x24, 0x20, 0x00];
+    spi_write(&mut di, &ngam_ctrl);
 
     // Digital Gamma Control (DGAM CTRL)
-    command_write(&di, 0xE2);
-    spi_write(&di, 0x0F);
-    spi_write(&di, 0x32);
-    spi_write(&di, 0x2E);
-    spi_write(&di, 0x0B);
-    spi_write(&di, 0x0D);
-    spi_write(&di, 0x05);
-    spi_write(&di, 0x47);
-    spi_write(&di, 0x75);
-    spi_write(&di, 0x37);
-    spi_write(&di, 0x06);
-    spi_write(&di, 0x10);
-    spi_write(&di, 0x03);
-    spi_write(&di, 0x24);
-    spi_write(&di, 0x20);
-    spi_write(&di, 0x00);
+    command_write(&mut di, 0xE2);
+    spi_write(&mut di, &ngam_ctrl);
 
-    command_write(&di, 0x11);
-    delay.delay_ms(150);
-    command_write(&di, 0x29);
+    // Inv off
+    command_write(&mut di, 0x20);
+
+    // Memory format
+    command_write(&mut di, 0x36);   
+    spi_write(&mut di, &[0x48]);
+
+    command_write(&mut di, 0x29);
 
     delay.delay_ms(150);
 
-    command_write(&di, 0x2A);
-    spi_write(&di, 0x00);
-    spi_write(&di, 0xFF);
-    spi_write(&di, 0x20);
-    spi_write(&di, 0xFF);
+    command_write(&mut di, ILI9468_MAD_BGR | ILI9468_MAD_MX);
 
-    command_write(&di, 0x2B);
-    spi_write(&di, 0x00);
-    spi_write(&di, 0xFF);
-    spi_write(&di, 0x20);
-    spi_write(&di, 0xFF);
+    let start_x = 0u16;
+    let end_x = 200u16;
 
-    command_write(&di, 0x2C);
+    command_write(&mut di, 0x2A);
+    let mut pager: [u16; 4] = [0x00, start_x, 0x00, end_x];
+    spi_write(&mut di, &pager);
 
-    let mut i = 0;
-    while(i<32*32){
-        i+=1;
-        spi_write(&di, 0x0F);
-    }
+    let start_y = 0u16;
+    let end_y = 200u16;
 
+    let mut column: [u16; 4] = [0x00; 4];
+    column[0] = 0x00;
+    column[1] = 0x00;
+
+    column[2] = 0x00;
+    column[3] = end_y;
+
+    // column[0..2].copy_from_slice(&start_y.to_be_bytes());
+    // column[2..4].copy_from_slice(&end_y.to_be_bytes());
+
+
+    command_write(&mut di, 0x2B);
+    spi_write(&mut di, &column);
+
+    command_write(&mut di, 0x2C);
+    let graphics: [u16; 200*200] = [0x0F; 200*200];
+    spi_write(&mut di, &graphics);
 
 
     loop {
